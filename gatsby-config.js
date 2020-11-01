a6fa4d9334b453109bf5317d06364c30156046c8
M. Brent Harp <brharp@uoguelph.ca>
2020-11-01 14:00:47 -0500

    Set source to live

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-let metaConfig = require('./config/sites/' + process.env._SITE + '.js');
+//let metaConfig = require('./config/sites/' + process.env._SITE + '.js');
 // uncomment the line below (and comment out the line above) for dev purposes:
-// let metaConfig = require('./config/sites/ugconthub.js');
+let metaConfig = require('./config/sites/ugconthub.js');
 
 if((metaConfig == null) || (metaConfig == undefined)) {
   metaConfig['title'] = "Gatsby UG Starter Template";
@@ -58,9 +58,9 @@ module.exports = {
     {
        resolve: `gatsby-source-drupal`,
        options: {
-         baseUrl: `https://api.` + process.env._GATSBY_SOURCE + process.env._SITE + `.` + process.env._ZONE + `/`,
+         //baseUrl: `https://api.` + process.env._GATSBY_SOURCE + process.env._SITE + `.` + process.env._ZONE + `/`,
 		 // uncomment the line below (and comment out the line above) for dev purposes:
-		 //baseUrl: `https://api.devugconthub.uoguelph.dev/`,
+	 baseUrl: `https://api.liveugconthub.uoguelph.dev/`,
          apiBase: `jsonapi`, // optional, defaults to `jsonapi`
        },
     },    
